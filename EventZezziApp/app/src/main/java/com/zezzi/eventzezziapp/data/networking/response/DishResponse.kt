73ecddf6f4a2bae476a098e908da636399dc8284
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-10-21 17:25:38 -0600

    Ya funcionan los datos del nuevo filtro de la api

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/response/DishResponse.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/response/DishResponse.kt
@@ -2,7 +2,7 @@ package com.zezzi.eventzezziapp.data.networking.response
 
 import com.google.gson.annotations.SerializedName
 
-data class DishCategoriesResponse(val filter: List<DishResponse>)
+data class DishCategoriesResponse(@SerializedName("meals") val filter: List<DishResponse>)
 
 data class DishResponse(
     @SerializedName("strMeal") val name1: String,
